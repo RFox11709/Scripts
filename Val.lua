38cb1bac3f30c86b12a72e0daca0f6cb5b909677
RFox11709 <158549340+RFox11709@users.noreply.github.com>
2025-01-06 21:30:18 +0630

    Update Val.lua

--- Val.lua
+++ Val.lua
@@ -2,10 +2,10 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 -- Creating the main Window
 local Window = Rayfield:CreateWindow({
-   Name = "Ghost's Hack GUI",
+   Name = "Val",
    Icon = 0,
-   LoadingTitle = "Loading Interface...",
-   LoadingSubtitle = "Powered by Rayfield",
+   LoadingTitle = "Loading...",
+   LoadingSubtitle = "Made by Ghost",
    Theme = "Default",
    ConfigurationSaving = {
       Enabled = true,
@@ -14,43 +14,111 @@ local Window = Rayfield:CreateWindow({
    }
 })
 
--- Creating the first Tab
-local Test1Tab = Window:CreateTab("Test1", 4483362458)
+-- Player Control Tab
+local PlayerTab = Window:CreateTab("Player", 4483362458)
 
--- Adding the first Button in Test1
-local Button1 = Test1Tab:CreateButton({
-   Name = "Noti",
+-- Walk Speed Changer Button
+local WalkSpeedButton = PlayerTab:CreateButton({
+   Name = "Walk Speed Changer",
    Callback = function()
-      Rayfield:Notify({
-         Title = "Notification",
-         Content = "hehe",
-         Duration = 5
-      })
+      local player = game.Players.LocalPlayer
+      if player and player.Character and player.Character:FindFirstChild("Humanoid") then
+         player.Character.Humanoid.WalkSpeed = 100  -- Change to your desired speed
+      end
    end
 })
 
--- Adding the second Button in Test1
-local Button2 = Test1Tab:CreateButton({
-   Name = "Another Button",
+-- Jump Power Changer Button
+local JumpPowerButton = PlayerTab:CreateButton({
+   Name = "Jump Power Changer",
    Callback = function()
-      print("Button in Test1 clicked!")
+      local player = game.Players.LocalPlayer
+      if player and player.Character and player.Character:FindFirstChild("Humanoid") then
+         player.Character.Humanoid.JumpPower = 100  -- Change to your desired jump power
+      end
    end
 })
 
--- Creating the second Tab
-local Test2Tab = Window:CreateTab("Test2", 4483362458)
-
--- Adding Buttons in Test2
-local Button3 = Test2Tab:CreateButton({
-   Name = "Test2 Button1",
-   Callback = function()
-      print("Button1 in Test2 clicked!")
+-- Fly Toggle Button
+local FlyToggle = PlayerTab:CreateToggle({
+   Name = "Fly",
+   CurrentValue = false,
+   Flag = "FlyToggle", -- Flag for configuration saving
+   Callback = function(Value)
+      local player = game.Players.LocalPlayer
+      if player and player.Character then
+         if Value then
+            -- Simple flying method
+            local bodyVelocity = Instance.new("BodyVelocity")
+            bodyVelocity.MaxForce = Vector3.new(400000, 400000, 400000)
+            bodyVelocity.Velocity = Vector3.new(0, 50, 0)
+            bodyVelocity.Parent = player.Character:WaitForChild("HumanoidRootPart")
+         else
+            -- Remove flying if toggled off
+            local bodyVelocity = player.Character:FindFirstChildOfClass("BodyVelocity")
+            if bodyVelocity then
+               bodyVelocity:Destroy()
+            end
+         end
+      end
    end
 })
 
-local Button4 = Test2Tab:CreateButton({
-   Name = "Test2 Button2",
-   Callback = function()
-      print("Button2 in Test2 clicked!")
+-- Visual Tab
+local VisualTab = Window:CreateTab("Visual", 4483362458)
+
+-- Fullbright Toggle Button
+local FullbrightToggle = VisualTab:CreateToggle({
+   Name = "Fullbright",
+   CurrentValue = false,
+   Flag = "FullbrightToggle", -- Flag for configuration saving
+   Callback = function(Value)
+      if Value then
+         -- Activate Fullbright (set lighting to maximum)
+         game.Lighting.Brightness = 2
+         game.Lighting.OutdoorAmbient = Color3.fromRGB(255, 255, 255)
+         game.Lighting.Ambient = Color3.fromRGB(255, 255, 255)
+      else
+         -- Reset to default lighting
+         game.Lighting.Brightness = 1
+         game.Lighting.OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+         game.Lighting.Ambient = Color3.fromRGB(128, 128, 128)
+      end
+   end
+})
+
+-- Player ESP Toggle Button
+local ESPToggle = VisualTab:CreateToggle({
+   Name = "Player ESP",
+   CurrentValue = false,
+   Flag = "ESPToggle", -- Flag for configuration saving
+   Callback = function(Value)
+      local player = game.Players.LocalPlayer
+      for _, v in pairs(game.Players:GetPlayers()) do
+         if v ~= player then
+            -- Create ESP for other players
+            if Value then
+               local esp = Instance.new("BillboardGui")
+               esp.Parent = v.Character:WaitForChild("Head")
+               esp.Adornee = v.Character:WaitForChild("Head")
+               esp.Size = UDim2.new(0, 100, 0, 50)
+               esp.StudsOffset = Vector3.new(0, 2, 0)
+               esp.AlwaysOnTop = true
+               local textLabel = Instance.new("TextLabel")
+               textLabel.Parent = esp
+               textLabel.Text = v.Name
+               textLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+               textLabel.BackgroundTransparency = 1
+               textLabel.Size = UDim2.new(1, 0, 1, 0)
+            else
+               -- Remove ESP
+               for _, gui in pairs(v.Character:WaitForChild("Head"):GetChildren()) do
+                  if gui:IsA("BillboardGui") then
+                     gui:Destroy()
+                  end
+               end
+            end
+         end
+      end
    end
 })
